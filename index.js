a8cb170b23e5863f7dc0a8e333075fffa831cc5e
ankit123-cool <55777905+ankit123-cool@users.noreply.github.com>
2022-12-22 11:24:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "userdb",
+});
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/userinsert", (req, res) => {
+  const Username = req.body.Username;
+  const Usermail = req.body.Usermail;
+  const Useraddress = req.body.Useraddress;
+  const Usercontact = req.body.Usercontact;
+  const Userdob = req.body.Userdob;
+
+  const sqlinsert =
+    "INSERT INTO user (Username,Usermail,Useraddress,Usercontact,Userdob) VALUES (?,?,?,?,?)";
+  db.query(
+    sqlinsert,
+    [Username, Usermail, Useraddress, Usercontact, Userdob],
+    (err, result) => {
+      res.status(200).json("hi");
+    }
+  );
+});
+
+app.listen(3001, () => {
+  console.log("running");
+});
